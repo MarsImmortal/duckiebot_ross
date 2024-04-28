bd25029f378860d21b2fff6f620cdc6471276647
Oscar-Stonesy42 <OscarStones2427@gmail.com>
2024-04-28 19:15:43 +1000

    modify open loop py file for p task

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -56,22 +56,22 @@ class Drive_Square:
     # Move the robot forward by a specified distance (side_length)
     def move_forward(self, distance):
         self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.2  # Forward velocity (adjust as needed)
+        self.cmd_msg.v = 0.1  # Forward velocity (adjust as needed)
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
         rospy.loginfo(f"Moving Forward by {distance} meters...")
-        rospy.sleep(distance / 0.2)
-        self.stop_robot()  # Adjust sleep time based on the robot's speed and required distance
+        rospy.sleep(distance / 0.1)
+        #self.stop_robot()  # Adjust sleep time based on the robot's speed and required distance
 
     def turn_robot(self):
         # Set the angular velocity to turn 90 degrees (adjust as needed)
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
-        self.cmd_msg.omega = 1  # Adjust the angular velocity for a slower turn
+        self.cmd_msg.omega = 0.5  # Adjust the angular velocity for a slower turn
         self.pub.publish(self.cmd_msg)
         rospy.loginfo("Turning...")
-        rospy.sleep(0.75)
-        self.stop_robot()  # Adjust sleep time for a 90-degree turn based on the turning speed
+        rospy.sleep(1.57)
+        #self.stop_robot()  # Adjust sleep time for a 90-degree turn based on the turning speed
 
 
     # Run the ROS node (spin forever)
