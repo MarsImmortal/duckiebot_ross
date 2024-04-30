94d078a7609a92d5559116e4adb2ae78900ba062
MarsImmortal <222198007@deakin.edu.au>
2024-05-01 02:27:13 +1000

    move straight function

--- packages/open_loop_square/src/ticktest.py
+++ packages/open_loop_square/src/ticktest.py
@@ -9,12 +9,11 @@ class Drive_Square:
         # Initialize ROS node
         rospy.init_node('drive_square_node', anonymous=True)
 
-
-        # Initialize Pub/Subs
+        # Initialize Publishers and Subscribers
         self.publisher = rospy.Publisher('/oryx/right_wheel_encoder_node/tick', WheelEncoderStamped, queue_size=10)
         self.pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         rospy.Subscriber('/oryx/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
-        rospy.Subscriber('/oryx/right_wheel_encoder_node/tick', WheelEncoderStamped, self.encoder_callback,self.publish_encoder_data, queue_size=1)
+        rospy.Subscriber('/oryx/right_wheel_encoder_node/tick', WheelEncoderStamped, self.encoder_callback, queue_size=1)
 
     def fsm_callback(self, msg):
         rospy.loginfo("State: %s", msg.state)
@@ -23,28 +22,24 @@ class Drive_Square:
         if msg.state == "NORMAL_JOYSTICK_CONTROL":
             self.stop_robot()  # Stop the robot if in joystick control mode
         elif msg.state == "LANE_FOLLOWING":
-            rospy.sleep(1)  # Wait for a second for the node to be ready # Calibrate ticks per meter
-            self.turn_robot(90)# Move the robot forward by 1 meter
-            self.stop_robot() # Stop the robot if in joystick control mode
+            rospy.sleep(1)  # Wait for a second for the node to be ready
+            self.turn_robot(90)  # Turn the robot by 90 degrees
+            self.stop_robot()  # Stop the robot after turning
 
     def encoder_callback(self, msg):
         # Store initial ticks value upon receiving first encoder message
         rospy.loginfo("Ticks: %s", msg.data)
-        
-    def publish_encoder_data(self, degree, msg):
-    # Create a WheelEncoderStamped message
-        encoder_msg = WheelEncoderStamped()
-        target_degree = msg.data + degree
-        # Populate the message fields (e.g., header, data, resolution, type)
-        encoder_msg.header.stamp = rospy.Time.now()  # Use current time as the message timestamp
-        encoder_msg.header.frame_id = "oryx/right_wheel_axis"
-        encoder_msg.data  = target_degree
-        encoder_msg.resolution = 135
-        encoder_msg.type = 1
 
-        # Publish the message
+    def publish_encoder_data(self, degree, msg):
+        # Create a copy of the received message to modify
+        encoder_msg = msg
+        # Calculate the target degree
+        target_degree = msg.data + degree
+        # Update the message data with the new target degree
+        encoder_msg.data = target_degree
+        # Publish the modified message
         self.publisher.publish(encoder_msg)
-        rospy.loginfo("Published encoder data")
+        rospy.loginfo(f"Published encoder data: {target_degree}")
 
     def stop_robot(self):
         # Send zero velocities to stop the robot
@@ -53,12 +48,11 @@ class Drive_Square:
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
         rospy.loginfo("Robot Stopped")
-        
-    def turn_robot(self, degrees):
-        self.publish_encoder_data(degrees)
-        rospy.loginfo(f"Turning Right by {degrees} degrees...")
-        
 
+    def turn_robot(self, degrees):
+        # Publish the desired degree change
+        rospy.loginfo(f"Turning Right by {degrees} degrees...")
+        self.publish_encoder_data(degrees)
 
     def run(self):
         rospy.spin()  # Keeps the node from exiting until shutdown
