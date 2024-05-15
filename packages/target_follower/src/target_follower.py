d80082c5fbb6ef964322c5c315c6fd145d0133c1
Oscar Stones <s222263094@deakin.edu.au>
2024-05-15 14:00:30 +1000

    modify 7 week task

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -2,35 +2,39 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
 
 class Target_Follower:
     def __init__(self):
-        
-        #Initialize ROS node
+        # Initialize the ROS node
         rospy.init_node('target_follower_node', anonymous=True)
 
-        # When shutdown signal is received, we run clean_shutdown function
+        # Setup shutdown function
         rospy.on_shutdown(self.clean_shutdown)
         
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
-        self.cmd_vel_pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
-        ################################################################
+        # Publisher and subscriber initialization
+        self.cmd_vel_pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/oryx/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
+        
+        # Start spinning the robot
+        self.keep_spinning()
 
-        rospy.spin() # Spin forever but listen to message callbacks
+        # Start the ROS loop
+        rospy.spin()
 
-    # Apriltag Detection Callback
+    # Callback function for AprilTag detections
     def tag_callback(self, msg):
-        self.move_robot(msg.detections)
- 
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+        if len(msg.detections) > 0:
+            self.stop_robot()
+        else:
+            self.keep_spinning()
+
+    # Stop the robot safely on shutdown
     def clean_shutdown(self):
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
 
-    # Sends zero velocity to stop the robot
+    # Method to stop the robot completely
     def stop_robot(self):
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
@@ -38,28 +42,13 @@ class Target_Follower:
         cmd_msg.omega = 0.0
         self.cmd_vel_pub.publish(cmd_msg)
 
-    def move_robot(self, detections):
-
-        #### YOUR CODE GOES HERE ####
-
-        if len(detections) == 0:
-            return
-
-        x = detections[0].transform.translation.x
-        y = detections[0].transform.translation.y
-        z = detections[0].transform.translation.z
-
-        rospy.loginfo("x,y,z: %f %f %f", x, y, z)
-
-
-        # Publish a velocity
+    # Method to continuously spin the robot
+    def keep_spinning(self):
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0
-        cmd_msg.omega = 0.0
+        cmd_msg.v = 0  # No forward movement
+        cmd_msg.omega = 1.0  # Constant angular velocity for spinning
         self.cmd_vel_pub.publish(cmd_msg)
-
-        #############################
 
 if __name__ == '__main__':
     try:
