37672834e7e95136ee3ba75dcf4dbd17fc768ca7
Oscar Stones <s222263094@deakin.edu.au>
2024-05-15 13:22:29 +1000

    inital setup for task 7.1

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -2,84 +2,67 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
 
-class TargetFollowerFSM:
+class Target_Follower:
     def __init__(self):
-        rospy.init_node('target_follower_fsm_node', anonymous=True)
+        
+        #Initialize ROS node
+        rospy.init_node('target_follower_node', anonymous=True)
 
-        self.cmd_vel_pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/oryx/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
+        # When shutdown signal is received, we run clean_shutdown function
+        rospy.on_shutdown(self.clean_shutdown)
+        
+        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
+        self.cmd_vel_pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/akandb/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
+        ################################################################
 
-        self.rate = rospy.Rate(10)  # Control loop rate (Hz)
+        rospy.spin() # Spin forever but listen to message callbacks
 
-        self.Kp = 0.3  # Proportional gain for angular control
-
-        self.target_detected = False
-        self.tag_position_x = 0.0
-
-        self.state = 'idle'
-        self.run_fsm()
-
+    # Apriltag Detection Callback
     def tag_callback(self, msg):
-        if msg.detections:
-            self.target_detected = True
-            self.tag_position_x = msg.detections[0].transform.translation.x
-        else:
-            self.target_detected = False
+        self.move_robot(msg.detections)
+ 
+    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+    def clean_shutdown(self):
+        rospy.loginfo("System shutting down. Stopping robot...")
+        self.stop_robot()
 
-    def run_fsm(self):
-        while not rospy.is_shutdown():
-            if self.state == 'idle':
-                self.handle_idle_state()
-            elif self.state == 'tracking':
-                self.handle_tracking_state()
-            elif self.state == 'lost':
-                self.handle_lost_state()
-            else:
-                rospy.logerr("Invalid state!")
-
-            self.rate.sleep()
-
-    def handle_idle_state(self):
-        if self.target_detected:
-            rospy.loginfo("Tag detected. Transitioning to tracking state.")
-            self.state = 'tracking'
-
-    def handle_tracking_state(self):
-        if not self.target_detected:
-            rospy.loginfo("Tag lost. Transitioning to lost state.")
-            self.state = 'lost'
-            return
-
-        # Calculate error
-        target_x = 0.0  # Center of camera frame
-        x_error = target_x - self.tag_position_x
-
-        # Calculate angular velocity based on error
-        omega = self.Kp * x_error
-
-        # Publish velocity command
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0  # Linear velocity
-        cmd_msg.omega = omega
-        self.cmd_vel_pub.publish(cmd_msg)
-
-    def handle_lost_state(self):
-        if self.target_detected:
-            rospy.loginfo("Tag found. Transitioning back to tracking state.")
-            self.state = 'tracking'
-
-        # Stop the robot
+    # Sends zero velocity to stop the robot
+    def stop_robot(self):
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
         cmd_msg.v = 0.0
         cmd_msg.omega = 0.0
         self.cmd_vel_pub.publish(cmd_msg)
 
+    def move_robot(self, detections):
+
+        #### YOUR CODE GOES HERE ####
+
+        if len(detections) == 0:
+            return
+
+        x = detections[0].transform.translation.x
+        y = detections[0].transform.translation.y
+        z = detections[0].transform.translation.z
+
+        rospy.loginfo("x,y,z: %f %f %f", x, y, z)
+
+
+        # Publish a velocity
+        cmd_msg = Twist2DStamped()
+        cmd_msg.header.stamp = rospy.Time.now()
+        cmd_msg.v = 0.0
+        cmd_msg.omega = 0.0
+        self.cmd_vel_pub.publish(cmd_msg)
+
+        #############################
+
 if __name__ == '__main__':
     try:
-        TargetFollowerFSM()
+        target_follower = Target_Follower()
     except rospy.ROSInterruptException:
         pass
