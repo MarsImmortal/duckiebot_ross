1229d949c4e614eafc97539b5334ddc937117cdb
MarsImmortal <222198007@deakin.edu.au>
2024-06-07 18:55:32 +1000

    experiment with detection system

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import AprilTagDetectionArray
 
-class TargetFollower:
+class Target_Follower:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('target_follower_node', anonymous=True)
@@ -14,8 +16,8 @@ class TargetFollower:
         self.cmd_vel_pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         rospy.Subscriber('/oryx/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
         
-        # Flag to indicate if AprilTag is detected
-        self.tag_detected = False
+        # Start spinning the robot
+        self.keep_spinning()
 
         # Start the ROS loop
         rospy.spin()
@@ -23,10 +25,8 @@ class TargetFollower:
     # Callback function for AprilTag detections
     def tag_callback(self, msg):
         if len(msg.detections) > 0:
-            self.tag_detected = True
             self.stop_robot()
         else:
-            self.tag_detected = False
             self.keep_spinning()
 
     # Stop the robot safely on shutdown
@@ -44,15 +44,14 @@ class TargetFollower:
 
     # Method to continuously spin the robot
     def keep_spinning(self):
-        if not self.tag_detected:
-            cmd_msg = Twist2DStamped()
-            cmd_msg.header.stamp = rospy.Time.now()
-            cmd_msg.v = 0  # No forward movement
-            cmd_msg.omega = 1.0  # Constant angular velocity for spinning
-            self.cmd_vel_pub.publish(cmd_msg)
+        cmd_msg = Twist2DStamped()
+        cmd_msg.header.stamp = rospy.Time.now()
+        cmd_msg.v = 0  # No forward movement
+        cmd_msg.omega = 1.0  # Constant angular velocity for spinning
+        self.cmd_vel_pub.publish(cmd_msg)
 
 if __name__ == '__main__':
     try:
-        target_follower = TargetFollower()
+        target_follower = Target_Follower()
     except rospy.ROSInterruptException:
         pass
