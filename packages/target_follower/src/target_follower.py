7680ec73b8226ef82dcc12b6494f337e20e2dbe5
MarsImmortal <222198007@deakin.edu.au>
2024-06-07 17:01:04 +1000

    experiment with detection system

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import AprilTagDetectionArray
 
-class Target_Follower:
+class TargetFollower:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('target_follower_node', anonymous=True)
@@ -16,8 +14,8 @@ class Target_Follower:
         self.cmd_vel_pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         rospy.Subscriber('/oryx/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
         
-        # Start spinning the robot
-        self.keep_spinning()
+        # Flag to indicate if AprilTag is detected
+        self.tag_detected = False
 
         # Start the ROS loop
         rospy.spin()
@@ -25,8 +23,12 @@ class Target_Follower:
     # Callback function for AprilTag detections
     def tag_callback(self, msg):
         if len(msg.detections) > 0:
+            self.tag_detected = True
+            tag_ids = [tag.id for tag in msg.detections]
+            rospy.loginfo("AprilTag detected with IDs: %s" % tag_ids)
             self.stop_robot()
         else:
+            self.tag_detected = False
             self.keep_spinning()
 
     # Stop the robot safely on shutdown
@@ -44,14 +46,15 @@ class Target_Follower:
 
     # Method to continuously spin the robot
     def keep_spinning(self):
-        cmd_msg = Twist2DStamped()
-        cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0  # No forward movement
-        cmd_msg.omega = 2.0  # Constant angular velocity for spinning
-        self.cmd_vel_pub.publish(cmd_msg)
+        if not self.tag_detected:
+            cmd_msg = Twist2DStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.v = 0  # No forward movement
+            cmd_msg.omega = 1.0  # Constant angular velocity for spinning
+            self.cmd_vel_pub.publish(cmd_msg)
 
 if __name__ == '__main__':
     try:
-        target_follower = Target_Follower()
+        target_follower = TargetFollower()
     except rospy.ROSInterruptException:
         pass
