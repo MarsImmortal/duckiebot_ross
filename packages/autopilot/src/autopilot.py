43e4307d41d83da5561376a05d45d0ed0e1db72a
MarsImmortal <222198007@deakin.edu.au>
2024-06-10 02:47:31 +1000

    credit task initial commit

--- packages/autopilot/src/autopilot.py
+++ packages/autopilot/src/autopilot.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
 
 import rospy
-from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
-from duckietown_msgs.msg import AprilTagDetectionArray
+from duckietown_msgs.msg import Twist2DStamped, FSMState, AprilTagDetectionArray
+from duckietown_msgs.msg import AprilTagDetection
 
 class Autopilot:
     def __init__(self):
         
-        #Initialize ROS node
+        # Initialize ROS node
         rospy.init_node('autopilot_node', anonymous=True)
 
         self.robot_state = "LANE_FOLLOWING"
+        self.ignore_apriltag = False
 
         # When shutdown signal is received, we run clean_shutdown function
         rospy.on_shutdown(self.clean_shutdown)
         
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
-        self.cmd_vel_pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        self.state_pub = rospy.Publisher('/akandb/fsm_node/mode', FSMState, queue_size=1)
-        rospy.Subscriber('/akandb/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
+        ###### Init Pub/Subs. REMEMBER TO REPLACE "oryx" WITH YOUR ROBOT'S NAME #####
+        self.cmd_vel_pub = rospy.Publisher('/oryx/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        self.state_pub = rospy.Publisher('/oryx/fsm_node/mode', FSMState, queue_size=1)
+        rospy.Subscriber('/oryx/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
         ################################################################
 
         rospy.spin() # Spin forever but listen to message callbacks
 
     # Apriltag Detection Callback
     def tag_callback(self, msg):
-        if self.robot_state != "LANE_FOLLOWING":
+        if self.robot_state != "LANE_FOLLOWING" or self.ignore_apriltag:
             return
         
         self.move_robot(msg.detections)
  
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+    # Stop Robot before node has shut down. This ensures the robot doesn't keep moving with the latest velocity command
     def clean_shutdown(self):
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
@@ -52,21 +52,38 @@ class Autopilot:
         self.state_pub.publish(state_msg)
 
     def move_robot(self, detections):
-
-        #### YOUR CODE GOES HERE ####
-
         if len(detections) == 0:
             return
 
-        # self.set_state("NORMAL_JOYSTICK_CONTROL") # Stop Lane Following
+        # Example: Stop at Stop Sign
+        for detection in detections:
+            if detection.id == 1:  # Assuming tag ID 1 is the stop sign
+                rospy.loginfo("Stop sign detected. Stopping the robot...")
+                self.set_state("NORMAL_JOYSTICK_CONTROL") # Stop Lane Following
+                self.stop_robot()
+                rospy.sleep(3)  # Stop for 3 seconds
 
-        # Process AprilTag info and publish a velocity
-        
-        # For open loop control, consider using rospy.sleep() afterwards.
-        
-        #self.set_state("LANE_FOLLOWING") # Go back to lane following
+                # Move forward a bit to ensure the stop sign is out of view
+                cmd_msg = Twist2DStamped()
+                cmd_msg.header.stamp = rospy.Time.now()
+                cmd_msg.v = 0.5  # Move forward with a velocity of 0.5
+                cmd_msg.omega = 0.0
+                self.cmd_vel_pub.publish(cmd_msg)
+                rospy.sleep(2)  # Move forward for 2 seconds
 
-        #############################
+                # Stop the robot again
+                self.stop_robot()
+
+                # Ignore AprilTag messages for 5 seconds to avoid immediate re-triggering
+                self.ignore_apriltag = True
+                rospy.sleep(5)
+                self.ignore_apriltag = False
+
+                # Resume lane following
+                self.set_state("LANE_FOLLOWING")
+                rospy.loginfo("Resuming lane following...")
+
+                return
 
 if __name__ == '__main__':
     try:
