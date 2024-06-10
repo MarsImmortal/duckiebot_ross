e3034323ecfedc5dc0eda487fb6e13c37904c71a
MarsImmortal <222198007@deakin.edu.au>
2024-06-11 02:28:11 +1000

    final commit

--- packages/my_lane_detector/src/my_lane_detector.py
+++ packages/my_lane_detector/src/my_lane_detector.py
@@ -64,11 +64,11 @@ class LaneDetector:
                 cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 2)
 
         # Display the processed images in separate windows
-        cv2.imshow('Lane Detection - Cropped Image', cropped_img)
-        cv2.imshow('Lane Detection - White Mask', white_mask)
-        cv2.imshow('Lane Detection - Yellow Mask', yellow_mask)
-        cv2.imshow('Lane Detection - orignal', img)
-        cv2.imshow('Canny Edge Detection', edges)
+        cv2.imshow('crop', cropped_img)
+        cv2.imshow('white', white_mask)
+        cv2.imshow('yellow', yellow_mask)
+        cv2.imshow('orignal image', img)
+        cv2.imshow('edge detection', edges)
         cv2.waitKey(1)
 
     def run(self):
